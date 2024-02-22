22164c309f9747935e58ba74ef67c0042d66eea9
nyaatsuki <puttinpanfu@gmail.com>
2024-02-22 12:21:50 +0100

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weblog</title>
+    <link rel="stylesheet" href="/stylesheet.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="title">
+            <h1>Blog title goes here</h1>
+            <a>Blog blurb goes here</a>
+        </div>
+        <div class="login">
+            <a>Account</a>
+        </div>
+    </div>
+
+
+    <div class="content">
+        <a>EMPY :C</a>
+    </div>
+
+</body>
+
+</html>
