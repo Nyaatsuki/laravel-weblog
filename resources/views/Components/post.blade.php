e5bfc8834366fee686f6cda5d300c421a869b785
nyaatsuki <puttinpanfu@gmail.com>
2024-03-05 11:40:43 +0100

    Post split into blade component

---
+++ resources/views/Components/post.blade.php
@@ -0,0 +1,10 @@
+<article class="post-container">
+    <img src="https://i.redd.it/bm2svk0ndu771.jpg" class="small-img">
+    <div class="container-title">
+        <h3>Post title goes here</h3>
+        <span>published <time>1 day ago</time></span>
+    </div>
+    <p>Sint veniam sint quis ipsum officia elit aliqua elit cupidatat. Aliquip consequat commodo aliquip minim dolore sit fugiat commodo nulla magna nostrud ad. Magna in voluptate ut amet laborum sint reprehenderit irure voluptate id. Consectetur qui nisi non aliqua voluptate quis fugiat. Sunt nulla minim amet labore consectetur ipsum non nulla nulla dolore qui.
+
+        Labore ut ea ut fugiat cupidatat. Velit non tempor ea mollit. Quis in pariatur pariatur qui est et.</p>
+</article>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,71 +3,15 @@
 
     <!--TODO: Foreach logic to display posts-->
     <div class="content">
-        <article class="featured-post-container">
-            <img src="https://i.redd.it/bm2svk0ndu771.jpg" class="featured-img">
-            <div class="featured-text">
-                <div class="container-title">
-                    <h3>Post title goes here</h3>
-                    <span>published <time>1 day ago</time></span>
-                </div>
-                <p>Sint veniam sint quis ipsum officia elit aliqua elit cupidatat. Aliquip consequat commodo aliquip minim dolore sit fugiat commodo nulla magna nostrud ad. Magna in voluptate ut amet laborum sint reprehenderit irure voluptate id. Consectetur qui nisi non aliqua voluptate quis fugiat. Sunt nulla minim amet labore consectetur ipsum non nulla nulla dolore qui.
-
-                    Labore ut ea ut fugiat cupidatat. Velit non tempor ea mollit. Quis in pariatur pariatur qui est et.</p>
-            </div>
-        </article>
+        <x-featured-post></x-featured-post>
         <div class="grid-col-2">
-            <article class="post-container">
-                <img src="https://i.redd.it/bm2svk0ndu771.jpg" class="small-img">
-                <div class="container-title">
-                    <h3>Post title goes here</h3>
-                    <span>published <time>1 day ago</time></span>
-                </div>
-                <p>Sint veniam sint quis ipsum officia elit aliqua elit cupidatat. Aliquip consequat commodo aliquip minim dolore sit fugiat commodo nulla magna nostrud ad. Magna in voluptate ut amet laborum sint reprehenderit irure voluptate id. Consectetur qui nisi non aliqua voluptate quis fugiat. Sunt nulla minim amet labore consectetur ipsum non nulla nulla dolore qui.
-
-                    Labore ut ea ut fugiat cupidatat. Velit non tempor ea mollit. Quis in pariatur pariatur qui est et.</p>
-            </article>
-            <article class="post-container">
-                <img src="https://i.redd.it/bm2svk0ndu771.jpg" class="small-img">
-                <div class="container-title">
-                    <h3>Post title goes here</h3>
-                    <span>published <time>1 day ago</time></span>
-                </div>
-                <p>Sint veniam sint quis ipsum officia elit aliqua elit cupidatat. Aliquip consequat commodo aliquip minim dolore sit fugiat commodo nulla magna nostrud ad. Magna in voluptate ut amet laborum sint reprehenderit irure voluptate id. Consectetur qui nisi non aliqua voluptate quis fugiat. Sunt nulla minim amet labore consectetur ipsum non nulla nulla dolore qui.
-
-                    Labore ut ea ut fugiat cupidatat. Velit non tempor ea mollit. Quis in pariatur pariatur qui est et.</p>
-            </article>
+            <x-post></x-post>
+            <x-post></x-post>
         </div>
         <div class="grid-col-3">
-            <article class="post-container">
-                <img src="https://i.redd.it/bm2svk0ndu771.jpg" class="small-img">
-                <div class="container-title">
-                    <h3>Post title goes here</h3>
-                    <span>published <time>1 day ago</time></span>
-                </div>
-                <p>Sint veniam sint quis ipsum officia elit aliqua elit cupidatat. Aliquip consequat commodo aliquip minim dolore sit fugiat commodo nulla magna nostrud ad. Magna in voluptate ut amet laborum sint reprehenderit irure voluptate id. Consectetur qui nisi non aliqua voluptate quis fugiat. Sunt nulla minim amet labore consectetur ipsum non nulla nulla dolore qui.
-
-                    Labore ut ea ut fugiat cupidatat. Velit non tempor ea mollit. Quis in pariatur pariatur qui est et.</p>
-            </article>
-            <article class="post-container">
-                <img src="https://i.redd.it/bm2svk0ndu771.jpg" class="small-img">
-                <div class="container-title">
-                    <h3>Post title goes here</h3>
-                    <span>published <time>1 day ago</time></span>
-                </div>
-                <p>Sint veniam sint quis ipsum officia elit aliqua elit cupidatat. Aliquip consequat commodo aliquip minim dolore sit fugiat commodo nulla magna nostrud ad. Magna in voluptate ut amet laborum sint reprehenderit irure voluptate id. Consectetur qui nisi non aliqua voluptate quis fugiat. Sunt nulla minim amet labore consectetur ipsum non nulla nulla dolore qui.
-
-                    Labore ut ea ut fugiat cupidatat. Velit non tempor ea mollit. Quis in pariatur pariatur qui est et.</p>
-            </article>
-            <article class="post-container">
-                <img src="https://i.redd.it/bm2svk0ndu771.jpg" class="small-img">
-                <div class="container-title">
-                    <h3>Post title goes here</h3>
-                    <span>published <time>1 day ago</time></span>
-                </div>
-                <p>Sint veniam sint quis ipsum officia elit aliqua elit cupidatat. Aliquip consequat commodo aliquip minim dolore sit fugiat commodo nulla magna nostrud ad. Magna in voluptate ut amet laborum sint reprehenderit irure voluptate id. Consectetur qui nisi non aliqua voluptate quis fugiat. Sunt nulla minim amet labore consectetur ipsum non nulla nulla dolore qui.
-
-                    Labore ut ea ut fugiat cupidatat. Velit non tempor ea mollit. Quis in pariatur pariatur qui est et.</p>
-            </article>
+            <x-post></x-post>
+            <x-post></x-post>
+            <x-post></x-post>
         </div>
         <!--<a>No posts yet, come back later!</a>-->
     </div>
