2662664c162abc7a45c01b71bdd0f2b450a03615
nyaatsuki <puttinpanfu@gmail.com>
2024-03-12 13:25:08 +0100

    spacing fix

--- resources/views/Components/post.blade.php
+++ resources/views/Components/post.blade.php
@@ -12,7 +12,7 @@
         <div class="container-author">
             <img class="author-image" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png">
             <div>
-                <span>written By&nbsp</span>
+                <span>written By</span>
                 <strong><a>{{$post->author->name}}</a></strong>
             </div>
         </div>
