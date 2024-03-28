c049367adf3e5dbd0fe8129514d8fb22a5f8c833
nyaatsuki <puttinpanfu@gmail.com>
2024-03-28 12:51:15 +0100

    article tooltip

--- resources/views/Components/article-creator.blade.php
+++ resources/views/Components/article-creator.blade.php
@@ -1,6 +1,9 @@
 @auth
 @if(! Request::is('create-article'))
 <div class="create-article">
+    <span class="article-tooltip">Write a new article
+        <div></div>
+    </span>
     <a href="/create-article">+</a>
 </div>
 @endif
