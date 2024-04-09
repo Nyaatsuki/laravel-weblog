3ee11ebd18ab155e0fdf551d5620994e0cf4b55c
nyaatsuki <puttinpanfu@gmail.com>
2024-04-09 15:09:49 +0200

    form fix

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="article-creator">
         <form method="POST" action="/create-article">
-            <textarea></textarea>
+            <textarea name="text"></textarea>
             <button class="submit-btn" type="submit">Save</button>
         </form>
     </div>
