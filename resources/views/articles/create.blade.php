f431c13575824e48c2de520b0fab89b3857f1ffb
nyaatsuki <puttinpanfu@gmail.com>
2024-04-18 13:39:15 +0200

    styling post creator

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,17 +1,13 @@
 <x-layout>
-    <div class="article-creator">
+    <section class="article-creator">
         <form method="POST" action="/create-article">
             @csrf
-
-            <label for="name">Title</label><br>
-            <input type="text" id="title" name="title"><br>
-            <textarea name="body" id="body"></textarea>
+            <input type="text" id="title" name="title" placeholder="Title">
+            <textarea name="body" id="body" placeholder="Post Body"></textarea>
+            <p>@foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach</p>
             <button class="submit-btn" type="submit">Save</button>
         </form>
-        
-        @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-
-    </div>
+    </section>
 </x-layout>
