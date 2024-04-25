26fcdca0aacb65904921cc9fd8afb69a08dad25b
nyaatsuki <puttinpanfu@gmail.com>
2024-04-25 11:14:00 +0200

    fix register page

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,14 +16,14 @@
                 @error('username')
                 <p style="color:red; font-size:12px;">{{ $message }}</p>
                 @enderror
-                <label for="password"> Password: </label><br>
-                <input type="password" id="password" name="password"><br>
-                @error('password')
-                <p style="color:red; font-size:12px;">{{ $message }}</p>
-                @enderror
                 <label for="email"> Email: </label><br>
                 <input type="text" id="email" name="email" value="{{ old('email') }}"><br>
                 @error('email')
+                <p style="color:red; font-size:12px;">{{ $message }}</p>
+                @enderror
+                <label for="password"> Password: </label><br>
+                <input type="password" id="password" name="password"><br>
+                @error('password')
                 <p style="color:red; font-size:12px;">{{ $message }}</p>
                 @enderror
                 <button type="submit">Submit</button>
