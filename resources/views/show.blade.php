5ec1418888edb52a4e44201df2b1c136dd2261ba
nyaatsuki <puttinpanfu@gmail.com>
2024-04-25 14:10:17 +0200

    various fixes

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,7 @@
 <title>Programmer's Syphon | <?= $post->title; ?></title>
 <x-layout>
     <div class="show-container">
-        <h1>{{$post->title}}</h1>
+        <h1 class="show-title" >{{$post->title}}</h1>
         <div class="show-category">
             <a href="/?category={{ $post->category->slug }}&{{ http_build_query(request()->except('category')) }}">{{$post->category->name}}</a>
         </div>
