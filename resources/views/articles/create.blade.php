eafc4a337987d884ef708508f347daff4f9802e8
nyaatsuki <puttinpanfu@gmail.com>
2024-04-30 13:27:29 +0200

    change save button

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -6,8 +6,11 @@
             <textarea name="body" id="body" placeholder="Post Body"></textarea>
             <p>@foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
-            @endforeach</p>
-            <button class="submit-btn" type="submit">Save</button>
+                @endforeach
+            </p>
+            <button class="submit-btn" type="submit">&#128428;<span class="article-tooltip">Save your article
+                    <div></div>
+                </span></button>
         </form>
     </section>
 </x-layout>
