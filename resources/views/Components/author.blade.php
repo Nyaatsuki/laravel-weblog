aedb2fe7dbc707a810f42e1550fcdf7423b8bded
nyaatsuki <puttinpanfu@gmail.com>
2024-07-30 13:15:11 +0200

    fix avatar link

--- resources/views/Components/author.blade.php
+++ resources/views/Components/author.blade.php
@@ -1,5 +1,5 @@
 <div class="container-author">
-    <img class="author-image" src="img/{{ $post->author->avatar }}">
+    <img class="author-image" src="/img/{{ $post->author->avatar }}">
     <div>
         <span>written By</span>
         <strong><a href="/?author={{ $post->author->username }}&{{ http_build_query(request()->except('author')) }}">{{$post->author->name}}</a></strong>
--- resources/views/Components/header.blade.php
+++ resources/views/Components/header.blade.php
@@ -17,7 +17,7 @@
         @auth
         <div class="avatar">
             <a href="/?author={{ auth()->user()->username }}&{{ http_build_query(request()->except('author')) }}">
-                <img src="img/{{ auth()->user()->avatar }}">
+                <img src="/img/{{ auth()->user()->avatar }}">
             </a>
         </div>
         <!--TODO: add this to a dropdown menu-->
