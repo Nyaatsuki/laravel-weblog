9657fcf84be555dd0371467e41e224580d076d80
nyaatsuki <puttinpanfu@gmail.com>
2024-08-01 11:44:23 +0200

    minor spelling mistake :nerd:

--- resources/views/Components/author.blade.php
+++ resources/views/Components/author.blade.php
@@ -1,7 +1,7 @@
 <div class="container-author">
     <img class="author-image" src="/img/{{ $post->author->avatar }}">
     <div>
-        <span>written By</span>
+        <span>Written By</span>
         <strong><a href="/?author={{ $post->author->username }}&{{ http_build_query(request()->except('author')) }}">{{$post->author->name}}</a></strong>
     </div>
 </div>
