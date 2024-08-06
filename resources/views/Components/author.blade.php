c71d080c2b4791b4dde07e7a49a424bbcd7486c7
nyaatsuki <puttinpanfu@gmail.com>
2024-08-06 11:44:05 +0200

    cleaner written notif PLACEHOLDER

--- resources/views/Components/author.blade.php
+++ resources/views/Components/author.blade.php
@@ -1,7 +1,20 @@
 <div class="container-author">
     <img class="author-image" src="/img/{{ $post->author->avatar }}">
     <div>
+        @auth
+
+        @if ( auth()->user()->id === $post->author->id )
+        <span>You wrote this,</span>
+        @else
         <span>Written By</span>
+        @endif
+
+        @endauth
+
+        @guest
+        <span>Written By</span>
+        @endguest
+        
         <strong><a href="/?author={{ $post->author->username }}&{{ http_build_query(request()->except('author')) }}">{{$post->author->name}}</a></strong>
     </div>
 </div>
--- resources/views/Components/container-footer.blade.php
+++ resources/views/Components/container-footer.blade.php
@@ -1,12 +1,5 @@
 @props(['post'])
 <div class="container-footer">
-    @auth
-    <div>
-        @if ( auth()->user()->id === $post->author->id )
-        <a>You wrote this!!!</a>
-        @endif
-    </div>
-    @endauth
     <x-author :post="$post" />
     <a href="posts/{{$post->slug}}" class="read-btn">
         Read More
