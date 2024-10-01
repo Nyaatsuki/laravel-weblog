397953964765ce8212d638dfc20529fed16ea88c
nyaatsuki <puttinpanfu@gmail.com>
2024-10-01 11:58:54 +0200

    small adjustements

--- resources/views/Components/author.blade.php
+++ resources/views/Components/author.blade.php
@@ -1,20 +1,7 @@
 <div class="container-author">
     <img class="author-image" src="{{ $post->author->avatar }}">
     <div>
-        @auth
-
-        @if ( auth()->user()->id === $post->author->id )
-        <span>You wrote this,</span>
-        @else
         <span>Written By</span>
-        @endif
-
-        @endauth
-
-        @guest
-        <span>Written By</span>
-        @endguest
-        
         <strong><a href="/?author={{ $post->author->username }}&{{ http_build_query(request()->except('author')) }}">{{$post->author->name}}</a></strong>
     </div>
 </div>
