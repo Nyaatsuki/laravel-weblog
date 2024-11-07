41c48033fc5877bb1bb79ba1e409f3540948c896
nyaatsuki <puttinpanfu@gmail.com>
2024-11-07 15:40:52 +0100

    Fix 419 error on comment page

--- resources/views/Components/comments/create-comment.blade.php
+++ resources/views/Components/comments/create-comment.blade.php
@@ -1,6 +1,7 @@
 <section class="comment-creator">
 <!-- TODO: Functionality to comment form, keep in mind comments need to be unique to their post -->
     <form method="POST" action="/comment" enctype="multipart/form-data">
+        @csrf
         <textarea name="comment-text" id="comment-text" placeholder="Type your comment here" rows="3"></textarea>
         <button>Reply</button>
     </form>
