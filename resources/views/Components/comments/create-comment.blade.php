ff23acb4c67892cbfbdee8bee4f09cf469c85e50
nyaatsuki <puttinpanfu@gmail.com>
2024-11-21 14:27:59 +0100

    spacing

--- resources/views/Components/comments/create-comment.blade.php
+++ resources/views/Components/comments/create-comment.blade.php
@@ -1,12 +1,12 @@
 <section class="comment-creator">
-<!-- TODO: Functionality to comment form, keep in mind comments need to be unique to their post -->
+    <!-- TODO: Functionality to comment form, keep in mind comments need to be unique to their post -->
     <form method="POST" action="/comment" enctype="multipart/form-data">
         @csrf
         <textarea name="comment-text" id="comment-text" placeholder="Type your comment here" rows="3"></textarea>
         <input type="hidden" name="post-id" id="post-id" value="{{ $post->id }}">
         <button>Reply</button>
         <p style="color:red;">@foreach ($errors->all() as $error)
-                <a>{{ $error }}</a>
-                @endforeach
+            <a>{{ $error }}</a>
+            @endforeach
     </form>
 </section>
